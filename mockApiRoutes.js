6f2546b6d56e9ac5f62c8b19b858f56347df9816
Meghana033 <meghanamadireddy7@gmail.com>
2026-05-28 18:46:59 +0530

    mockApiRoutes.js

--- mockApiRoutes.js
+++ mockApiRoutes.js
@@ -1,17 +1,32 @@
 import express from "express";
 
 import {
-    getUser,
-    getUserPosts,
-    getRecommendations
-} from "../controllers/mockApiController.js";
+    fetchProfile,
+    fetchOrders,
+    fetchTrendingProducts
+} from "../controllers/serviceController.js";
 
-const router = express.Router();
+const serviceRouter = express.Router();
 
-router.get("/users/:id", getUser);
 
-router.get("/users/:id/posts", getUserPosts);
+// Profile Route
+serviceRouter.get(
+    "/profiles/:id",
+    fetchProfile
+);
 
-router.get("/recommendations", getRecommendations);
 
-export default router;
+// Orders Route
+serviceRouter.get(
+    "/profiles/:id/orders",
+    fetchOrders
+);
+
+
+// Trending Products Route
+serviceRouter.get(
+    "/products/trending",
+    fetchTrendingProducts
+);
+
+export default serviceRouter;
